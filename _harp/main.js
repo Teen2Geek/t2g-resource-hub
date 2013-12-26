61cfdc7439992a37692ad705314dd1847cb886d5
Krish Dholakiya <krishna.dholakiya@gmail.com>
2013-12-26 14:56:39 -0700

    Added backend section

--- _harp/main.js
+++ _harp/main.js
@@ -1,3 +1,11 @@
 $(document).ready(function() {
 	hljs.initHighlightingOnLoad();
+	$('.item.add').hover(function(){
+		$(this).addClass('animated');
+		$(this).addClass('pulse');
+	})
+	$('.item.add').mouseleave(function() {
+		$(this).removeClass('animated');
+		$(this).removeClass('pulse');
+	})
 });
